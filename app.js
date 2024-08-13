ee70de2acdeeecd726b0337c4b2c3300dc5bd1cc
Hareem Aman <hareem9927@gmail.com>
2024-08-13 11:53:17 +0500

    get

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
-import express from 'express';
-import fs from 'fs';
-import path from 'path';
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
 app.use(express.json());
 
 const dataFilePath = path.resolve('data.json');
 
+app.get('/jobs', (req, res) => {
+  if (fs.existsSync(dataFilePath)) {
+    const fileContent = fs.readFileSync(dataFilePath, 'utf-8');
+    res.json(JSON.parse(fileContent));
+  } else {
+    res.json([]);
+  }
+});
+
 app.post('/jobs', (req, res) => {
   const formData = req.body;
 
   if (!formData || !formData.title || !formData.location || !formData.salary) {
-    return res.status(400).send('Fill feilds');
+    return res.status(400).send('Fill fields');
   }
 
   let existingData = [];
